9d9ad5eb3aa8ddbe583fa7db867a81ef896513a4
charlie-2201 <manuagarwal1999@gmail.com>
2025-02-28 14:49:57 +0530

    New Changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,13 @@
 const Contact = () => {
     return (
         <div>
-        <h1>Contact Us</h1>
-        <h2>This is my number...</h2>
+            <h1 className="font-bold text-3xl p-4 m-4">Contact Us</h1>
+            <form>
+                <input type="text" placeholder="Name" className="border-2 border-gray-500 p-2 m-2" />
+                <input type="text" placeholder="Message" className="border-2 border-gray-500 p-2 m-2" />
+                <button className="bg-blue-500 text-white rounded-lg p-2 m-2">Submit</button>
+            </form>
+            
         </div>
     )
 }
